428b9a38a42fb4a26be277aa5b48b1b39aec53fd
Matteo Falco <matteo.falco1986@gmail.com>
2024-03-09 11:54:19 +0000

    Tutto funzionante

---
+++ Hotel/Models/ServerStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+
+namespace Hotel.Models
+{
+    public static class ServerStrings
+    {
+        public static string Url = "https://localhost:44367";
+    }
+}
